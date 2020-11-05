0a83f9b47b6fe5194e566496c41bd4e60ecf747b
Mateusz <m.wasik1303@gmail.com>
2020-11-05 10:02:24 +0100

    not that old stuff

---
+++ HTML + CSS + DB/kolorowanieWizytówek/wizytowka.js
@@ -0,0 +1,8 @@
+let imie = prompt("Podaj swoje imię");
+let nazwisko = prompt("Podaj swoje nazwisko");
+
+let i = "Imię: ";
+let n = "Nazwisko: ";
+
+document.write(i.fontcolor("red") + imie + "<br>");
+document.write(n.fontcolor("blue") + nazwisko);
---
+++ HTML + CSS + DB/testy/main.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "Hello World!" + myElement.innerHTML;
+
+var myElement = document.getElementById("intro");
