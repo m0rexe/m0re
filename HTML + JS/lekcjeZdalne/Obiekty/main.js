170d4fe1f0409948dc71a655a1b43724f7351d93
Mateusz <m.wasik1303@gmail.com>
2020-12-01 13:13:58 +0100

    Added 'Obiekty'

---
+++ HTML + JS/lekcjeZdalne/Obiekty/main.js
@@ -0,0 +1,68 @@
+// 1. Utwórz skrypt, który za pomocą okna dialogowego wczyta liczbę i obliczy jej pierwiastek, a wynik wyświetli w oknie przeglądarki
+//
+// let liczba = Number(prompt("Podaj liczbę"));
+// let pierwiastek = Math.sqrt(liczba);
+//
+// document.write("Pierwiastek z liczby " + liczba + " wynosi: " + pierwiastek);
+//
+// 2. Utwórz skrypt, który wygeneruje liczby z zakresu <1,50> dla dzwudziestoelementowej tablicy, wyszuka w niej wartości  podzielne przez 3, wyświetli je oraz obliczy średnią arytmetyczną
+//
+//
+// let tab = []
+// let i = 0
+//
+// while (i<20){
+// let losowa = Math.floor(Math.random()*50)
+// tab.push(losowa)
+// i = i+1
+// }
+//
+//
+//
+// document.write(tab)
+//
+// 3. Utwórz skrypt pobierający dowolny tekst, a następnie wyświetlający długość tego tekstu.
+//
+// let txt = prompt("Podaj tekst:")
+//
+// document.write('Długość tekstu: '+txt.length)
+//
+// 4.Utwórz skrypt pobierający dowolny tekst pisany dużymi i małymi literami, a następnie wyświetli ten tekst dużymi literami.
+//
+// let txt = prompt("Podaj tekst:")
+//
+// document.write(txt.toUpperCase())
+//
+// 5.Utwórz skrypt pobierający dowolny tekst pisany dużymi i małymi literami, a następnie wyświetli ten tekst małymi literami.
+//
+// let txt = prompt("Podaj tekst:")
+//
+// document.write(txt.toLowerCase())
+//
+// 6.Utwórz skrypt który z łańcucha o twoim imieniu i nazwisku, wytnie i wyświetli tylko twoje imię.
+//
+// let imieNazwisko = "Mateusz Wąsik"
+//
+// document.write(imieNazwisko.substring(0,7))
+//
+// 7.Utwórz skrypt wyświetlający aktualną datę i godzinę.
+//
+// let data = new Date()
+//
+// document.write("Jest teraz godzina: "+ data.toLocaleTimeString())
+// document.write("<br>Dzisiejsza data: "+ data.toLocaleDateString())
+//
+// 8.Utwórz skrypt wyświetlający tylko aktualną datę.
+//
+// let data = new Date()
+//
+// document.write("Dzisiejsza data: "+ data.toLocaleDateString())
+//
+// 9.Utwórz skrypt wyświetlający tylko aktualną godzinę.
+
+// let data = new Date()
+
+// document.write("Jest teraz godzina: "+ data.toLocaleTimeString())
+
+
+// 10.Utwórz skrypt zawierający tablicę przechowującą imiona osób z grupy.
