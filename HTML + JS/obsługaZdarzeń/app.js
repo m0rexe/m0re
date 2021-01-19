427948fb606f1b99ca30449f0964c90daecaf30a
Mateusz Wąsik <m.wasik1303@gmail.com>
2021-01-19 12:01:00 +0100

    ^zdarzenia

--- HTML + JS/obsługaZdarzeń/app.js
+++ HTML + JS/obsługaZdarzeń/app.js
@@ -1,13 +1,11 @@
 // 3.86 str 99 - utworzenie przycku button i przypisanie zdarzenia onclick ✅
 // 3.87 str 99 - do buttona dodaj funkcję obsługująca zdarzenie - funkcję pisz i wypisz stosowny komunikat ✅
-// 3.88 str 101 - wykorzystanie zdarzenie onmouseover, onmouseout, onmousedown, grafikę potrzebną pobierz z pixaby.pl
-// 3.89 str 102 - po wciśnięciu przycisku myszy następuje zmiana grafiki.Po zwolnieniu przycisku następuje powrót do pierwotnej grafiki
+// 3.88 str 101 - wykorzystanie zdarzenie onmouseover, onmouseout, onmousedown, grafikę potrzebną pobierz z pixaby.pl ✅
+// 3.89 str 102 - po wciśnięciu przycisku myszy następuje zmiana grafiki.Po zwolnieniu przycisku następuje powrót do pierwotnej grafiki ✅
 
 function hello() {
 
     return alert('Witaj!');
 
 }
-
-
 
