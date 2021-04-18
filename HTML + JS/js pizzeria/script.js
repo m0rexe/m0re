b95d31d9366d1a8890c2c336e3c1695b68acd71a
Mateusz <m.wasik1303@gmail.com>
2021-04-18 14:39:05 +0200

    functions are great 😋

--- HTML + JS/js pizzeria/script.js
+++ HTML + JS/js pizzeria/script.js
@@ -14,120 +14,132 @@ oblicz = () => {
   let skladnik = 4;
   let dostawa = 7;
 
+
+  serowa = () => {
+    let koszt = cenaserowa * iloscpizz + dostawa;
+    document.getElementById('wynik').innerHTML =
+      'Koszt całkowity: ' + koszt + ' zł!';
+  }
+
+  serowa_uro = () => {
+    let koszt = cenaserowa * iloscpizz;
+    document.getElementById('wynik').innerHTML =
+      'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
+      koszt +
+      ' zł!';
+  }
+
+  peperoni = () => {
+    let koszt = (cenaserowa + skladnik) * iloscpizz + dostawa;
+    document.getElementById('wynik').innerHTML =
+      'Koszt całkowity: ' + koszt + ' zł!';
+  }
+
+  peperoni_uro = () => {
+    let koszt = (cenaserowa + skladnik) * iloscpizz;
+    document.getElementById('wynik').innerHTML =
+      'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
+      koszt +
+      ' zł!';
+  }
+
+  havai = () => {
+    let koszt = (cenaserowa + skladnik * 2) * iloscpizz + dostawa;
+    document.getElementById('wynik').innerHTML =
+      'Koszt całkowity: ' + koszt + ' zł!';
+  }
+
+  havai_uro = () => {
+    let koszt = (cenaserowa + skladnik * 2) * iloscpizz;
+    document.getElementById('wynik').innerHTML =
+      'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
+      koszt +
+      ' zł!';
+  }
+
+  kurczak = () => {
+    let koszt = (cenaserowa + skladnik * 3) * iloscpizz + dostawa;
+    document.getElementById('wynik').innerHTML =
+      'Koszt całkowity: ' + koszt + ' zł!';
+  }
+
+  kurczak_uro = () => {
+    let koszt = (cenaserowa + skladnik * 3) * iloscpizz;
+    document.getElementById('wynik').innerHTML =
+      'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
+      koszt +
+      ' zł!';
+  }
+
   switch (rodzaj) {
     case 'serowa':
       if (adres != 'nieWażne') {
         if (uro != 'tak') {
-          let koszt = cenaserowa * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
+          serowa()
         } else {
-          let koszt = cenaserowa * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
-        }
-      } else {
-        dostawa = 20;
-        if (uro != 'tak') {
-          let koszt = cenaserowa * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
-        } else {
-          let koszt = cenaserowa * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
-        }
-      }
-      break;
-    case 'peperoni':
-      if (adres != 'nieWażne') {
-        if (uro != 'tak') {
-          let koszt = (cenaserowa + skladnik) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
-        } else {
-          let koszt = (cenaserowa + skladnik) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
-        }
-      } else {
-        dostawa = 20;
-        if (uro != 'tak') {
-          let koszt = (cenaserowa + skladnik) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
-        } else {
-          let koszt = (cenaserowa + skladnik) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
-        }
-      }
-      break;
-    case 'havai':
-      if (adres != 'nieWażne') {
-        if (uro != 'tak') {
-          let koszt = (cenaserowa + skladnik * 2) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
-        } else {
-          let koszt = (cenaserowa + skladnik * 2) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
+          serowa_uro()
         }
       } else {
         if (uro != 'tak') {
           dostawa = 20;
-          let koszt = (cenaserowa + skladnik * 2) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
+          serowa()
         } else {
-          let koszt = (cenaserowa + skladnik * 2) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
+          serowa_uro()
         }
       }
       break;
+
+    case 'peperoni':
+      if (adres != 'nieWażne') {
+        if (uro != 'tak') {
+          peperoni()
+        } else {
+          peperoni_uro()
+        }
+      } else {
+        if (uro != 'tak') {
+          dostawa = 20;
+          peperoni()
+        } else {
+          peperoni_uro()
+        }
+      }
+      break;
+
+    case 'havai':
+      if (adres != 'nieWażne') {
+        if (uro != 'tak') {
+          havai()
+        } else {
+          havai_uro()
+        }
+      } else {
+        if (uro != 'tak') {
+          dostawa = 20;
+          havai()
+        } else {
+          havai_uro()
+        }
+      }
+      break;
+
     case 'kurczak':
       if (adres != 'nieWażne') {
         if (uro != 'tak') {
-          let koszt = (cenaserowa + skladnik * 3) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
+          kurczak()
         } else {
-          let koszt = (cenaserowa + skladnik * 3) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
+          kurczak_uro()
         }
       } else {
-        dostawa = 20;
-
         if (uro != 'tak') {
-          let koszt = (cenaserowa + skladnik * 3) * iloscpizz + dostawa;
-          document.getElementById('wynik').innerHTML =
-            'Koszt całkowity: ' + koszt + ' zł!';
+          dostawa = 20;
+          kurczak()
         } else {
-          let koszt = (cenaserowa + skladnik * 3) * iloscpizz;
-          document.getElementById('wynik').innerHTML =
-            'Masz dziś urodziny!!! <br> Koszt całkowity (bez dostawy): ' +
-            koszt +
-            ' zł!';
+          kurczak_uro()
         }
       }
       break;
+
     default:
       console.log('Something Went Wrong! O_o');
   }
