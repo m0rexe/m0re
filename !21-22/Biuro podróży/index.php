2569bacbb46bbac9a0526e536ef3491e7f08a563
Mateusz <m.wasik1303@gmail.com>
2022-02-14 23:50:57 +0100

    em something?

---
+++ !21-22/Biuro podróży/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Podróże dalekie i bliskie</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Biuro podróży: WESOŁA WYPRAWA</h1>
+    </header>
+    <div class="ciasteczko">
+
+        <p>test</p>
+
+    </div>
+    <div class="main">
+        <div class="left">
+            <table>
+                <tr>
+                    <td><img src="./img/polska.jpg" alt="zwiedzanie Krakowa"></td>
+                    <td><img src="./img/wlochy.jpg" alt="Wenecja i nie tylko"></td>
+                </tr>
+                <tr>
+                    <td><img src="./img/grecja.jpg" alt="gorące Greckie wyspy"></td>
+                    <td><img src="./img/hiszpania.jpg" alt="Słoneczna Barcelona"></td>
+                </tr>
+            </table>
+        </div>
+        <div class="right">
+            <div class="list">
+                <h3>Proponujemy wycieczki</h3>
+                <ul>
+                    <li>autokarem</li>
+                    <ol>
+                        <li>po Polsce z przewodnikiem</li>
+                        <li>do Niemiec i Czech</li>
+                    </ol>
+                    <li>samolotem</li>
+                    <ol>
+                        <li>wczasy w Grecji</li>
+                        <li>zwiedzanie Barcelony</li>
+                        <li>zwiedzanie Wenecji</li>
+                    </ol>
+                </ul>
+            </div>
+            <div class="docs">
+                <h3>Pobierz dokumenty</h3>
+                <a href="./regulamin.txt">
+                    <p>Regulamin korzystania z usług biura podróży</p>
+                </a>
+                <a href="http://galeria.pl/" target="_blank">
+                    <p>Tu znajdziesz zdjęcia z naszych wycieczek</p>
+                </a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>Stronę przygotował: 00000000000</p>
+    </footer>
+
+</body>
+
+</html>
